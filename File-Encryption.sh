5a9e8aa04693a020d99550fbcb6d76461caefb66
EgbewaleToluwani <egbewaletoluwani12@gmail.com>
2026-05-12 14:59:20 +0100

    Add files via upload

--- File-Encryption.sh
+++ File-Encryption.sh
@@ -4,11 +4,40 @@ echo "=========================================="
 echo "        Welcome to file encryption        "
 echo "=========================================="
 echo ""
-echo "Give your file a name: "
-read name
-echo "Give your file some content: "
-read content
-echo "Which type of encryption do you want? (Reply with the number only)"
+
+echo "Do you want to: (Reply with the number only!)"
+echo "1. Create a new file"
+echo "2. Import existing file"
+read mode
+
+if [ $mode = 1 ]; then 
+	echo "Give your file a name: "
+	read name
+	echo "Give your file some content: "
+	read content
+	echo $content > $name.txt
+	input_file=$name.txt
+
+elif [ $mode = 2 ]; then
+	echo "Enter the full path to your file: "
+	read input_file
+
+	#Check if file exists
+	if [ ! -f $input_file ]; then
+		echo "File not found!"
+		exit
+	fi
+
+	#Use the filename without extension as the archive name
+	name=$(basename "$input_file" | cut -d. -f1)
+	echo "File found: $input_file"
+else exit
+fi
+
+
+echo ""
+echo ""
+echo "Which type of encryption do you want?"
 echo "1. gzip"
 echo "2. bzip2"
 echo "3. tar archive"
@@ -22,41 +51,21 @@ echo ""
 
 
 if [ $compressionType = 1 ]; then
-echo $content | gzip > $name.gz
-echo "Done!"
+	gzip -c $input_file > $name.gz 
+	echo "Done! Created $name.gz"
 
 elif [ $compressionType = 2 ]; then
-echo $content | bzip2 > $name.bz2
-echo "Done!"
+	bzip2 -c $input_file > $name.bz2
+	echo "Done! Created $name.bz2"
 
 elif [ $compressionType = 3 ]; then
-echo $content > $name.txt
-tar -cf $name.tar $name.txt && rm $name.txt
-echo "Done!"
+	tar -cf $name.tar $input_file
+	echo "Done! Created $name.tar"
 
 elif [ $compressionType = 4 ]; then
-echo $content | zip > $name.zip
-echo "Done!"
+	zip $name.zip $input_file
+	echo "Done! Created $name.zip"
 
 else exit
 fi
-
-echo "Do you want to decompress?"
-echo "1. Yes"
-echo "2. No"
-read answer
-
-if [ $answer = 1 ] && [ $compressionType = 1 ]; then
-	gunzip $name.gz
-elif [ $answer = 1 ] && [ $compressionType = 2 ]; then
-	bunzip2 $name.bz2
-elif [ $answer = 1 ] && [ $compressionType = 3 ]; then
-	tar -xf $name.tar && rm $name.tar
-elif [ $answer = 1 ] && [ $compressionType = 4 ]; then
-	unzip -p $name.zip > $name.txt && rm $name.zip
-else exit
-fi
-
-
-
 
